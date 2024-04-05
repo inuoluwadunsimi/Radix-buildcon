0ac5bc76a20012833718b1e38ed6bb5704f64e27
Inuoluwadunsimi <danielolaoladeinde@gmail.com>
2024-04-05 14:10:50 +0100

    feat: remove message hash

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -15,11 +15,6 @@ export async function verifyMetMask(
 ): Promise<AuthResponse> {
   const { message, signature, address } = body;
   const prefixedMessage = `\x19Ethereum Signed Message:\n${message.length}${message}`;
-  const messageHash = web3.utils.sha3(prefixedMessage);
-  if (!messageHash) {
-    throw new BadRequestError("invalid message");
-  }
-  console.log(messageHash);
   const recoveredAddress = web3.eth.accounts.recover(
     prefixedMessage,
     signature
