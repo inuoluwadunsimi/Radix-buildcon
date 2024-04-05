bc959c0a5badb30373eebe8f23511526ecab9431
Inuoluwadunsimi <danielolaoladeinde@gmail.com>
2024-04-05 08:19:01 +0100

    chore: switch swagger config file to es5

--- src/configuration/swagger.ts
+++ src/configuration/swagger.ts
@@ -2,16 +2,15 @@ const swaggerJSDoc = require("swagger-jsdoc");
 
 const options = {
   definition: {
-    openapi: '3.0.0',
+    openapi: "3.0.0",
     info: {
-      title: 'typescript-express boilerplate',
-      version: '1.0.0',
-      description: 'Docs',
+      title: "radix api",
+      version: "1.0.0",
+      description: "Docs",
     },
   },
 
-  apis:['./spec/api.yaml']
+  apis: ["./spec/api.yaml"],
 };
 
-
-export default swaggerJSDoc(options)
+module.exports = swaggerJSDoc(options);
